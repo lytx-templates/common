d9c119b8fbebffcdcbca8d979c7dcbe7bbdd67ba
Glenn E. Bailey III <glenn.bailey@lytx.com>
2023-01-30 14:39:33 -0600

    Stuff

--- common.sh
+++ common.sh
@@ -5,3 +5,11 @@ DOCKER=docker
 CONFIG="makefile.json"
 
 source $HOME/common/docker/jq.sh
+
+if [ -n "$GITHUB_ACTIONS"  ]
+then
+    if [ ! -d "/runner/$GITHUB_RUN_ID" ]
+    then
+        mkdir /runner/$GITHUB_RUN_ID
+    fi
+fi
